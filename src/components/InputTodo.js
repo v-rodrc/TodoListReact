42dee3299e2bc4a4fe6bdae9edb469de77bdeda5
Chris Rodriguez <v-rodrc@microsoft.com>
2020-10-11 12:47:21 -0500

    new tasks now display in lists with unique ids

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -10,19 +10,30 @@ state = {
 //then calls onChange() class method and will rerender the state using setState method to the title 
 onChange = e => {
     this.setState({
-        title: e.target.value
+        [e.target.name]: e.target.value
+    });
+};
+
+handleSubmit = e => {
+    e.preventDefault();
+    this.props.addTodoProps(this.state.title);
+    this.setState({
+        title: ""
     });
 };
 
     render() {
         return (
-            <form>
-                <input type="text" 
+            <form onSubmit={this.handleSubmit}>
+                <input 
+                type="text" 
                 placeholder="Add todo..."
                 value={this.state.title}
+                name="title"
                 onChange={this.onChange}
                 />
-                <input type="submit" value="Submit"/>
+                <input type="submit" value="Submit"
+            />
             </form>
         )
     }
--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -2,6 +2,7 @@ import React from "react"
 import TodosList from "./TodosList"
 import Header from "./Header"
 import InputTodo from "./InputTodo"
+import { v4 as uuidv4 } from "uuid"
 
 class ToDoContainer extends React.Component {
 
@@ -10,17 +11,17 @@ class ToDoContainer extends React.Component {
     state = {
         todos: [
             {
-                id: 1,
+                id: uuidv4(),
                 title: "Setup development environment",
                 completed: true
             },
             {
-                id: 2,
+                id: uuidv4(),
                 title: "Develop website and add content",
                 copmleted: false
             },
             {
-                id: 3,
+                id: uuidv4(),
                 title: "Deploy to live server",
                 completed: false
             }
@@ -53,16 +54,28 @@ class ToDoContainer extends React.Component {
         })
     };
 
+    addTotoItem = title => {
+        const newTodo = {
+            id: uuidv4(),
+            title: title,
+            completed: false
+        };
+        this.setState({
+            todos: [...this.state.todos, newTodo]
+        });
+    };
+
     render() {
         return (
             //now have the state data in todos prop
             <div>
                 <Header/>
-                <InputTodo />
+                <InputTodo addTodoProps={this.addTotoItem}/>
                 <TodosList 
                 todos={this.state.todos}
                 handleChangeProps={this.handleChange}
                 deleteTodoProps={this.delTodo}
+                
                 />
                 
             </div>
