e0635cb46f42cb9445dd7c8ded0d9fbc2ba7e523
Chris Rodriguez <v-rodrc@microsoft.com>
2020-10-11 00:42:31 -0500

    Created todo container

---
+++ src/components/TodoContainer.js
@@ -0,0 +1,13 @@
+import React from "react"
+class ToDoContainer extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hell from create react app</h1>
+                <p>I am in a react component!</p>
+            </div>
+        )
+    }
+}
+
+export default ToDoContainer
