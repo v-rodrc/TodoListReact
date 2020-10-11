ad52b8fa00e397cb1d7446871401449224df24a0
Chris Rodriguez <v-rodrc@microsoft.com>
2020-10-11 00:53:53 -0500

    added state to ToDoContainer

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,10 +1,35 @@
 import React from "react"
 class ToDoContainer extends React.Component {
+
+    //states, array of objects
+    //defined todos in the state object
+    state = {
+        todos: [
+            {
+                id: 1,
+                title: "Setup development environment",
+                completed: true
+            },
+            {
+                id: 2,
+                title: "Develop website and add content",
+                copmleted: false
+            },
+            {
+                id: 3,
+                title: "Deploy to live server",
+                completed: false
+            }
+        ]
+    };
+//acccessed toodos in the render method
+//looped through the array of objects, and output each title
     render() {
         return (
             <div>
-                <h1>Hell from create react app</h1>
-                <p>I am in a react component!</p>
+                {this.state.todos.map(todo => (
+                    <li>{todo.title}</li>
+                ))}
             </div>
         )
     }
