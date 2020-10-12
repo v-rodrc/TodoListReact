16185d74b19f20be8bcd8d8aa79b6841bd90a163
Chris Rodriguez <v-rodrc@microsoft.com>
2020-10-11 20:08:54 -0500

    Updated header style

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,20 @@
 import React from 'react'
 
 const Header = () => {
+
+const headerStyle= {
+    padding: "20px 0",
+    lineHeight: "23m",
+}
+
     return (
-        <header>
-            <h1>Simple Todo App</h1>
-            <p>Please add to-dos item(s) through the input field</p>
+        <header style={headerStyle}>
+            <h1 style={{fontSize: "25px", marginBottom: "15px"}}>
+                Simple Todo App
+                </h1>
+            <p style={{fontSize: "19px"}}>
+                Please add to-dos item(s) through the input field
+                </p>
         </header>
     )
 }
