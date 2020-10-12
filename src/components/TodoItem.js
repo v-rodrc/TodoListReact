062f7cd0f9d9e35a95a616ac5468ce98e362838f
Chris Rodriguez <v-rodrc@microsoft.com>
2020-10-11 20:09:19 -0500

    Updated todo item properties

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,17 +2,31 @@ import React, { Component } from 'react'
 
 
 class TodoItem extends Component {
+
+    
     render() {
      
+        const completedStyle = {
+            fontStyle: "italic",
+            color: "#d35e0f",
+            opacity: 0.4,
+            textDecoration: "line-through",
+        }
+
+        const { completed, id, title } = this.props.todo
+
         return  <li className="todo-item"> 
             <input type="checkbox" 
-        checked={this.props.todo.completed}
-        onChange={() => this.props.handleChangeProps(this.props.todo.id)}
+        checked={completed}
+        onChange={() => this.props.handleChangeProps(id)}
         />
          <button onClick={() =>
-        this.props.deleteTodoProps(this.props.todo.id)}>Delete</button>
-        {this.props.todo.title}</li>
-        
+        this.props.deleteTodoProps(id)}>Delete</button>
+       
+       <span style={completed ? completedStyle : null}>
+        {title}
+        </span>
+        </li>
     }
 }
 
